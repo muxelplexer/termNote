da1fb7fbb6f1feaf97402a0adef7d5f7208efaf9
Terodom <sebastian.muxel@a1.net>
2018-11-21 22:54:29 +0100

    initial stuff

---
+++ CMakeFiles/termNote.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/termNote.dir/src/main.cpp.o"
+  "CMakeFiles/termNote.dir/src/note.cpp.o"
+  "termNote.pdb"
+  "termNote"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/termNote.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
