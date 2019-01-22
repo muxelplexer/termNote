3487def35c8c1f582ef1007867b946d3912d34f0
Александр Бантьев <balsoft@yandex.ru>
2019-01-23 01:13:01 +0300

    Switch to 4 spaces & add emacs config for it (#9)
    * Add default.nix for nix
    
    * Fix src path
    
    * Remove unpackPhase
    
    * Add support for XDG standarts
    (use XDG_CONFIG_HOME and XDG_DATA_HOME when available)
    
    * Add a helpful first-use note
    
    * Add noted, a notification daemon for termNote
    
    * Revert "Add noted, a notification daemon for termNote"
    
    This reverts commit 64a9c1ed2a00b05555e416162c56cc9d562b9968.
    
    * Add noted, a notification daemon for termNote
    
    * Fix noted
    
    * Fix markdown styling in README.md
    
    * Fix indentation
    
    * Remove unneeded includes & replace define with const char*
    
    * Switch to using 4 spaces & add emacs config for it

--- include/note.hpp
+++ include/note.hpp
@@ -1,13 +1,13 @@
 #pragma once
 #define GCC_VERSION (__GNUC__ * 10000 \
-										 + __GNUC_MINOR__ * 100 \
-										 + __GNUC_PATCHLEVEL__)
+                     + __GNUC_MINOR__ * 100 \
+                     + __GNUC_PATCHLEVEL__)
 #if GCC_VERSION < 80000
-	#include <experimental/filesystem>
-	namespace fs = std::experimental::filesystem;
+    #include <experimental/filesystem>
+    namespace fs = std::experimental::filesystem;
 #else
-	#include <filesystem>
-	namespace fs = std::filesystem;
+    #include <filesystem>
+    namespace fs = std::filesystem;
 #endif
 #include <memory>
 #include <iostream>
@@ -21,26 +21,26 @@
 #define XDG_DATA_HOME std::getenv("XDG_DATA_HOME")
 
 class Note {
-	private:
-		// Filestreams
-		std::fstream noteStream;
-		std::fstream tempStream;
-		
-		// Files and Directories
-		std::string configDir = XDG_CONFIG_HOME ? std::string(XDG_CONFIG_HOME) + "/termNote/" : std::string(HOME) + "/.config/termNote/";
-		std::string noteDir = XDG_DATA_HOME ? std::string(XDG_DATA_HOME) + "/termNote/" : std::string(HOME) + "/.termNote/";
-		std::string tempFile = noteDir + "tempNotes";
-		std::string file = noteDir + "notes";
-		std::string configFile = configDir + "config";
-			
-		int delLine;
-		std::string line;
-	public:
-		Note();
-		~Note();
+    private:
+        // Filestreams
+        std::fstream noteStream;
+        std::fstream tempStream;
+        
+        // Files and Directories
+        std::string configDir = XDG_CONFIG_HOME ? std::string(XDG_CONFIG_HOME) + "/termNote/" : std::string(HOME) + "/.config/termNote/";
+        std::string noteDir = XDG_DATA_HOME ? std::string(XDG_DATA_HOME) + "/termNote/" : std::string(HOME) + "/.termNote/";
+        std::string tempFile = noteDir + "tempNotes";
+        std::string file = noteDir + "notes";
+        std::string configFile = configDir + "config";
+            
+        int delLine;
+        std::string line;
+    public:
+        Note();
+        ~Note();
 
-		void add(char* note);
-		void del(int delLine);
-		void list();
-		std::vector<std::string> getList();
+        void add(char* note);
+        void del(int delLine);
+        void list();
+        std::vector<std::string> getList();
 };
--- src/note.cpp
+++ src/note.cpp
@@ -1,60 +1,60 @@
 #include "../include/note.hpp"
 
 Note::Note() {
-	// Look if termNote directories and files exist, if not then create them
-	if(fs::exists(this->noteDir) != 1) {
-		fs::create_directory(this->noteDir);
-	}
-	std::ifstream iFileTest(this->file);
-	if (!iFileTest.good()) {
-		std::ofstream oFileTest(this->file);
-		oFileTest << "This is a sample note. You can run `termNote --help` to get help. ";
-		oFileTest.close();
-	}
-	iFileTest.close();
+    // Look if termNote directories and files exist, if not then create them
+    if(fs::exists(this->noteDir) != 1) {
+        fs::create_directory(this->noteDir);
+    }
+    std::ifstream iFileTest(this->file);
+    if (!iFileTest.good()) {
+        std::ofstream oFileTest(this->file);
+        oFileTest << "This is a sample note. You can run `termNote --help` to get help. ";
+        oFileTest.close();
+    }
+    iFileTest.close();
 }
 
 Note::~Note() {
-	
+    
 }
 
 void Note::add(char* note) {
-	this->noteStream.open(this->file, std::ios::app);
-	noteStream << note << std::endl;
+    this->noteStream.open(this->file, std::ios::app);
+    noteStream << note << std::endl;
 }
 
 void Note::del(int delLine) {
-	this->noteStream.open(this->file, std::ios::in);
-	this->tempStream.open(this->tempFile, std::ios::out);
-	int i = 0;
-	while(std::getline(noteStream, line)) {
-		if(i != delLine) {
-			tempStream << line << "\n";
-		}
-		i++;
-	}
-	noteStream.close();
-	tempStream.close();
-	remove(this->file.c_str());
-	rename(this->tempFile.c_str(),this->file.c_str());
+    this->noteStream.open(this->file, std::ios::in);
+    this->tempStream.open(this->tempFile, std::ios::out);
+    int i = 0;
+    while(std::getline(noteStream, line)) {
+        if(i != delLine) {
+            tempStream << line << "\n";
+        }
+        i++;
+    }
+    noteStream.close();
+    tempStream.close();
+    remove(this->file.c_str());
+    rename(this->tempFile.c_str(),this->file.c_str());
 }
 void Note::list() {
-	noteStream.open(file, std::ios::in);
-	int i = 0;
-	while(std::getline(noteStream, line)) {
-		std::string newLine;
-		newLine = line + "\n";
-		std::cout <<"[" << i << "]"<< " " << newLine;
-		i++;
-	}
-	noteStream.close();
+    noteStream.open(file, std::ios::in);
+    int i = 0;
+    while(std::getline(noteStream, line)) {
+        std::string newLine;
+        newLine = line + "\n";
+        std::cout <<"[" << i << "]"<< " " << newLine;
+        i++;
+    }
+    noteStream.close();
 }
 
 std::vector<std::string> Note::getList() {
-	noteStream.open(file, std::ios::in);
-	std::vector<std::string> result;
-	while(std::getline(noteStream, line))
-		result.push_back(line);
-	noteStream.close();
-	return result;
+    noteStream.open(file, std::ios::in);
+    std::vector<std::string> result;
+    while(std::getline(noteStream, line))
+        result.push_back(line);
+    noteStream.close();
+    return result;
 }
